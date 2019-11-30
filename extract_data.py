d580903ac3bb48fb70b3430708c47469c15a43b2
lingsin5234 <sinto.005234@gmail.com>
2019-11-30 08:43:47 -0500

    10 spot checks for 2015 files are good.

--- extract_data.py
+++ extract_data.py
@@ -6,7 +6,7 @@ import pandas as pd
 import re
 
 # open and read data
-f = open("retrodata/2015BOS.EVA", "r")
+f = open("retrodata/2015TBA.EVA", "r")
 f1 = f.readlines()
 
 # test print
@@ -108,7 +108,7 @@ def convert_games(all_games):
 # re-write the processor based on re.search/re.findall grep searching
 def play_processor2(the_df):
 
-    print(the_df.index)
+    # print(the_df.index)
     # process would go line by line.
     for i in the_df.index:
 
@@ -116,7 +116,7 @@ def play_processor2(the_df):
         if the_df.at[i, 'type'] == 'play':
 
             # Case 1: regular single out plays
-            if re.search(r'^[1-9]([1-9]+)?(!)?/(G|F|L|P|BG|BP|BL)', the_df.at[i, 'play']):
+            if re.search(r'^[1-9]([1-9!]+)?/(G|F|L|P|BG|BP|BL)', the_df.at[i, 'play']):
                 # print('Routine Put Out: ', the_df.at[i, 'play'])
                 pass
 
@@ -225,9 +225,14 @@ def play_processor2(the_df):
                 pass
 
             # Case 23: unexpected runner advance
-            # elif re.search(r'^OA', the_df.at[i, 'play']):
+            elif re.search(r'^OA', the_df.at[i, 'play']):
                 # print('Unexpected Runner Adv.: ', the_df.at[i, 'play'])
-                # pass
+                pass
+
+            # Case 24: batter interference
+            elif re.search(r'([1-9]+)/BINT', the_df.at[i, 'play']):
+                # print('Batter Interference: ', the_df.at[i, 'play'])
+                pass
 
             # Case 20: ELSE
             else:
@@ -440,14 +445,14 @@ a_full_df = convert_games(games)
 # new_output = play_processor2(df1)
 # print(len(a_full_df))
 for e, each_game in enumerate(a_full_df):
-    # print('game #: ', e+1)
+    print('game #: ', e+1)
     # print(each_game.index)
     # print(a_full_df[e].index)
-    pass
+    # pass
     # df = a_full_df[e].copy()
     # print(df.index)
     # print(len(df))
-    # new_output = play_processor2(each_game)
+    new_output = play_processor2(each_game)
 
 # print(a_full_df[79])
 
