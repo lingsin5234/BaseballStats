87aff9cce13f1cba2da90021b27d43c14d8a42d4
lingsin5234 <sinto.005234@gmail.com>
2019-12-02 18:02:19 -0500

    one stat for 1 player for 1 year

--- extract_data.py
+++ extract_data.py
@@ -248,6 +248,9 @@ def play_processor2(game_num, the_df):
                     the_df.at[i, '3B_after'] = the_df.at[i, 'playerID']
                 else:
                     the_df.at[i, 'runs_scored'] += 1
+                    # log HR for donaldson
+                    if the_df.at[i, 'playerID'] == 'donaj001':
+                        stat_organizer(the_df.at[i, 'playerID'], game_num, 'home_run', 1)
 
             # Case 12: walk or hit by pitch
             elif re.search(r'^(HP|IW|W)(?!P)(?!\+)', the_df.at[i, 'play']):
@@ -506,201 +509,19 @@ def play_processor2(game_num, the_df):
     return the_df
 
 
-# batch play processor
-def play_processor(the_play):
-
-    # determine if out(s)
-    out_plays = tuple(''.join(map(str, list(range(1, 10))))) + ('K',)
-
-    # determine hits/walks
-    b1_plays = tuple(''.join('SW'))
-    b2_plays = tuple('D')
-    b3_plays = tuple('T')
-    hr_plays = tuple('H')
-
-    # put out(s)
-    if the_play['play'].startswith(out_plays):
-        if the_play['play'].endswith('DP'):
-            the_play.at['outs'] += 2
-        elif the_play['play'].endswith('TP'):
-            the_play.at['outs'] += 3
-        else:
-            the_play.at['outs'] += 1
-
-    # process batter ONLY
-    if the_play['play'].startswith(b1_plays):
-        # except WP - wild pitch
-        if re.search(r'^WP|SB', the_play['play']):
-            pass
-        else:
-            the_play.at['1B_after'] = the_play['playerID']
-    elif the_play['play'].startswith(b2_plays):
-        the_play.at['2B_after'] = the_play['playerID']
-    elif the_play['play'].startswith(b3_plays):
-        the_play.at['3B_after'] = the_play['playerID']
-    # process HR runs_scored ONLY
-    elif the_play['play'].startswith(hr_plays):
-        the_play.at['runs_scored'] += 1
-
-    # return
-    return the_play
+# use 1 player for now
+global player
+player = pd.DataFrame(columns=('player_id', 'game_id', 'stat_type', 'stat_value'))
 
 
-# process the baserunners
-def baserunner_processor(the_df):
-    # get all half innings in the game
-    all_half = the_df.half_innings.unique()
-    full_df = pd.DataFrame()
+# stat organizer
+def stat_organizer(player_id, game_id, stat_type, stat_value):
 
-    # process by half innings at a time
-    for half in all_half:
-        # each half inning
-        half_df = the_df[the_df.half_innings == half]
-        half_df = half_df.reset_index(drop=True)  # index needs to be reset to ref 0
-        prev_row = half_df.loc[0]
-        for i, d in half_df.iterrows():
+    # modify player table
+    player.loc[-1] = [player_id, game_id, stat_type, stat_value]
+    player.index = player.index + 1
 
-            # carry-over runners from previous line
-            if i > 0:
-                if prev_row['1B_after'] is not None:
-                    half_df.at[i, '1B_before'] = prev_row['1B_after']
-                if prev_row['2B_after'] is not None:
-                    half_df.at[i, '2B_before'] = prev_row['2B_after']
-                if prev_row['3B_after'] is not None:
-                    half_df.at[i, '3B_before'] = prev_row['3B_after']
-
-            # move the runners
-            if d['play'] is not None:
-                if re.search(r'\.', d['play']):
-                    if re.search(r'1-2', d['play']):
-                        half_df.at[i, '2B_after'] = half_df.loc[i, '1B_before']
-                    if re.search(r'1-3', d['play']):
-                        half_df.at[i, '3B_after'] = half_df.loc[i, '1B_before']
-                    if re.search(r'1-H', d['play']):
-                        half_df.at[i, 'runs_scored'] += 1
-                    if re.search(r'2-3', d['play']):
-                        half_df.at[i, '3B_after'] = half_df.loc[i, '2B_before']
-                    if re.search(r'2-H', d['play']):
-                        half_df.at[i, 'runs_scored'] += 1
-                    if re.search(r'3-H', d['play']):
-                        half_df.at[i, 'runs_scored'] += 1
-
-                    # check for non-moving runners that did not get out
-                    if half_df.loc[i, '1B_before'] is not None and not re.search(r'1-|1X', d['play']):
-                        half_df.at[i, '1B_after'] = half_df.loc[i, '1B_before']
-                    if half_df.loc[i, '2B_before'] is not None and not re.search(r'2-|2X', d['play']):
-                        half_df.at[i, '2B_after'] = half_df.loc[i, '2B_before']
-                    if half_df.loc[i, '3B_before'] is not None and not re.search(r'3-|3X', d['play']):
-                        half_df.at[i, '3B_after'] = half_df.loc[i, '3B_before']
-
-                elif re.search(r'^SB', d['play']):
-
-                    # stealing 2nd
-                    if re.search(r'^SB2', d['play']):
-                        half_df.at[i, '2B_after'] = half_df.loc[i, '1B_before']
-                        if re.search(r'\.', d['play']):
-                            if re.search(r'2-3', d['play']):
-                                half_df.at[i, '3B_after'] = half_df.loc[i, '2B_before']
-                            if re.search(r'2-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                            if re.search(r'3-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                        else:
-                            if half_df.loc[i, '3B_before'] is not None:
-                                half_df.at[i, '3B_after'] = half_df.loc[i, '3B_before']
-
-                    # stealing 3rd
-                    if re.search(r'^SB3', d['play']):
-                        half_df.at[i, '3B_after'] = half_df.loc[i, '2B_before']
-                        if re.search(r'\.', d['play']):
-                            if re.search(r'3-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                            if re.search(r'1-2', d['play']):
-                                half_df.at[i, '2B_after'] = half_df.loc[i, '1B_before']
-                            if re.search(r'1-3', d['play']):
-                                half_df.at[i, '3B_after'] = half_df.loc[i, '1B_before']
-                            if re.search(r'1-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                        else:
-                            if half_df.loc[i, '1B_before'] is not None:
-                                half_df.at[i, '1B_after'] = half_df.loc[i, '1B_before']
-
-                    # stealing home
-                    if re.search(r'^SBH', d['play']):
-                        half_df.at[i, 'runs_scored'] += 1
-                        if re.search(r'\.', d['play']):
-                            if re.search(r'1-2', d['play']):
-                                half_df.at[i, '2B_after'] = half_df.loc[i, '1B_before']
-                            if re.search(r'1-3', d['play']):
-                                half_df.at[i, '3B_after'] = half_df.loc[i, '1B_before']
-                            if re.search(r'1-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                            if re.search(r'2-3', d['play']):
-                                half_df.at[i, '3B_after'] = half_df.loc[i, '2B_before']
-                            if re.search(r'2-H', d['play']):
-                                half_df.at[i, 'runs_scored'] += 1
-                        else:
-                            if half_df.loc[i, '1B_before'] is not None:
-                                half_df.at[i, '1B_after'] = half_df.loc[i, '1B_before']
-                            if half_df.loc[i, '2B_before'] is not None:
-                                half_df.at[i, '2B_after'] = half_df.loc[i, '2B_before']
-
-                else:
-                    # if not 3 outs yet, and some runners did not move, find and keep them.
-                    if i > 0 and half_df.loc[i, 'outs'] < 3:
-                        if half_df.loc[i, '1B_before'] is not None:
-                            half_df.at[i, '1B_after'] = half_df.loc[i, '1B_before']
-                        if half_df.loc[i, '2B_before'] is not None:
-                            half_df.at[i, '2B_after'] = half_df.loc[i, '2B_before']
-                        if half_df.loc[i, '3B_before'] is not None:
-                            half_df.at[i, '3B_after'] = half_df.loc[i, '3B_before']
-
-            # for sub lines, copy all baserunners
-            elif d['type'] == 'sub':
-                half_df.at[i, '1B_after'] = half_df.loc[i, '1B_before']
-                half_df.at[i, '2B_after'] = half_df.loc[i, '2B_before']
-                half_df.at[i, '3B_after'] = half_df.loc[i, '3B_before']
-
-            # save as previous row
-            prev_row = half_df.loc[i]
-
-        # append half inning to full game
-        # print(half_df, '\nNext\n')
-        full_df = full_df.append(half_df)
-    return full_df
-
-
-# function to process all half innings in a game
-def half_inning_process(the_df):
-    # get all half innings in the game
-    all_half = the_df.half_innings.unique()
-    full_df = pd.DataFrame()
-
-    # process by half innings at a time
-    for half in all_half:
-        # print(half)
-        total_outs = 0
-        half_df = the_df[the_df.half_innings == half]
-        for i, d in half_df.iterrows():
-
-            # process outs
-            if d['outs'] == 1:
-                total_outs += 1
-                half_df.at[i, 'outs'] = total_outs
-
-            # process DP and TP
-            elif d['outs'] == 2:
-                total_outs += 2
-                half_df.at[i, 'outs'] = total_outs
-
-            elif d['outs'] == 3:
-                total_outs += 3
-                half_df.at[i, 'outs'] = total_outs
-
-        # append half inning to full game
-        # print(half_df, '\nNext\n')
-        full_df = full_df.append(half_df)
-    return full_df
+    return True
 
 
 # get argument
@@ -755,3 +576,5 @@ for e, each_game in enumerate(a_full_df):
 # write to file
 # output_df.to_csv('OUTPUT.csv', sep=',', index=False)
 full_output.to_csv('OUTPUT.csv', sep=',', index=False)
+
+print(player)
