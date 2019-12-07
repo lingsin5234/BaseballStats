ecfe0f77767f537d8ba370decee66a124739f2e8
lingsin5234 <sinto.005234@gmail.com>
2019-12-06 20:08:57 -0500

    game starts tracking by game id

--- stat_collector.py
+++ stat_collector.py
@@ -1,5 +1,6 @@
 # libraries
 import global_variables as gv
+import pandas as pd
 
 
 # stat collector
@@ -21,6 +22,27 @@ def stat_organizer(player_tb):
 
 
 # game start tracker
-def game_tracker():
+def game_tracker(all_starts, all_game_ids):
 
-    return
+    # get all the game ids
+    games_ids = pd.DataFrame(all_game_ids)
+    games_ids[0] = games_ids[0].str.replace('\n', '')
+    games_ids[0] = games_ids[0].str.replace('id,', '')
+
+    # sort all the game starts
+    games_dfs = []
+    for g in range(len(all_starts)):
+        # convert to table
+        pdf = pd.DataFrame(all_starts[g][-2])  # 2nd last item is all starting lineups
+        df1 = pd.concat([pdf[0].str.split(',', expand=True)], axis=1)
+
+        # remove line break in last column
+        df1[5] = df1[5].str.replace('\n', '')
+
+        # insert game_id column
+        df1.insert(0, 'game_id', games_ids.loc[g, 0])
+
+        # add to full game_dfs list
+        games_dfs.append(df1.copy())
+
+    return games_dfs
