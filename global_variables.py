bf94a1dfaa5c553f6ba6306e874b03102aa8f61f
lingsin5234 <sinto.005234@gmail.com>
2019-12-08 17:10:37 -0500

    save stats in wide format

--- global_variables.py
+++ global_variables.py
@@ -4,3 +4,4 @@ import pandas as pd
 # create global tables.
 player = pd.DataFrame(columns=('player_id', 'game_id', 'stat_type', 'stat_value'))
 game_roster = pd.DataFrame(columns=('game_id', 'which_team', 'player_id', 'fielding'))
+player_stats = pd.DataFrame()
