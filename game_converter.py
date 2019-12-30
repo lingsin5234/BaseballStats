ea5d719c4bd217261a8593fbffa1ba5a1a90d071
lingsin5234 <sinto.005234@gmail.com>
2019-12-30 14:43:50 -0500

    dictionary vs data frame performance

--- game_converter.py
+++ game_converter.py
@@ -26,22 +26,61 @@ def convert_games(all_games):
                 dct = {'type': i[0],
                        'inning': i[1],
                        'half': i[2],
+                       'half_inning': i[1] + '_' + i[2],
                        'playerID': i[3],
                        'count': i[4],
                        'pitches': i[5],
-                       'play': i[6]}
+                       'play': i[6],
+                       'name': None,
+                       'team_id': i[2],
+                       'team_name': None,
+                       'batting': None,
+                       'fielding': None,
+                       'pitcherID': None,
+                       'outs': 0,
+                       '1B_before': None,
+                       '2B_before': None,
+                       '3B_before': None,
+                       '1B_after': None,
+                       '2B_after': None,
+                       '3B_after': None,
+                       'runs_scored': 0,
+                       'total_scored': 0}
             else:
                 dct = {'type': i[0],
+                       'inning': None,
+                       'half': None,
+                       'half_inning': None,
                        'playerID': i[1],
+                       'count': None,
+                       'pitches': None,
+                       'play': None,
                        'name': i[2],
                        'team_id': i[3],
+                       'team_name': None,
                        'batting': i[4],
-                       'fielding': i[5]}
+                       'fielding': i[5],
+                       'pitcherID': None,
+                       'outs': 0,
+                       '1B_before': None,
+                       '2B_before': None,
+                       '3B_before': None,
+                       '1B_after': None,
+                       '2B_after': None,
+                       '3B_after': None,
+                       'runs_scored': 0,
+                       'total_scored': 0}
             game_dict[idx] = dct
             idx += 1
 
         print(game_dict)
         exit()
+
+        #  ## performance analysis: DICTIONARY ##
+        d2_time = t.time()
+
+        #  ## performance analysis: DATA FRAME ##
+        t1_time = t.time()
 
         # convert to table
         pdf = pd.DataFrame(all_games[g][-1])  # last item is entire game_play
@@ -97,6 +136,13 @@ def convert_games(all_games):
         # insert half innings
         df1.insert(3, 'half_innings', None)
         df1.half_innings = df1.inning + '_' + df1.half
+
+        #  ## performance analysis: DATA FRAME ##
+        t2_time = t.time()
+
+        # ## performance analysis results ##
+        print('DICTIONARY: ', d2_time - d1_time)
+        print('DATA FRAME: ', t2_time - t1_time)
 
         # add to full game_dfs list
         games_dfs.append(df1.copy())
