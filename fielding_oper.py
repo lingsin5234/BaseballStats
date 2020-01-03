406c452f4505da629f42fac5302d6b96053e167a
lingsin5234 <sinto.005234@gmail.com>
2020-01-03 14:45:26 -0500

    testing fielding subs

--- fielding_oper.py
+++ fielding_oper.py
@@ -17,9 +17,17 @@ def lineup_substitution(this_line, lineup, pid, sub_type):
             exit()
 
     # check which spot in the lineup, get the playerID:
-    sub_filter = (lineup.team_id == this_line['team_id']) & \
-                 (lineup.bat_lineup == this_line[sub_type])
-    sub_index = lineup.index[sub_filter]
+    if sub_type == 'batting':
+        sub_filter = (lineup.team_id == this_line['team_id']) & \
+                     (lineup.bat_lineup == this_line[sub_type])
+        sub_index = lineup.index[sub_filter]
+    elif sub_type == 'fielding':
+        sub_filter = (lineup.team_id == this_line['team_id']) & \
+                     (lineup.fielding == this_line[sub_type])
+        sub_index = lineup.index[sub_filter]
+    else:
+        print('Substitution Error: Fielding Oper')
+        exit()
 
     # replace the person in the lineup
     lineup.at[sub_index[0], 'player_id'] = pid
