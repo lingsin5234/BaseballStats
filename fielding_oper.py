65cd0da4eae6195623a1c84314a378fcbf949ddc
lingsin5234 <sinto.005234@gmail.com>
2020-01-03 15:38:23 -0500

    check if new sub

--- fielding_oper.py
+++ fielding_oper.py
@@ -3,34 +3,39 @@ import global_variables as gv
 
 
 # fielding substitutions
-def lineup_substitution(this_line, lineup, pid, sub_type):
+def lineup_substitution(this_line, lineup, pid, sub_type, field_pos):
 
     # by default, all substitutions are new except when handling Fielding Subs
     new_substitution = True
 
     # if fielding substitution, check if person is already in lineup
     if sub_type == 'fielding':
-        if pid in lineup.player_id:
+        if pid in lineup.player_id.values:
             new_substitution = False
             print(this_line)
             print(lineup)
             exit()
+        if pid in lineup.values:
+            print('Works')
+            exit()
+        if pid == 'gutif001':
+            print(pid in lineup.player_id)
+            print(type(lineup))
+            print(type(pid))
+            print(type(lineup.player_id[1]))
+            print(lineup.player_id)
+            print(pid, lineup.player_id[1])
+            print(pid == lineup.player_id[1])
+            exit()
 
     # check which spot in the lineup, get the playerID:
-    if sub_type == 'batting':
-        sub_filter = (lineup.team_id == this_line['team_id']) & \
-                     (lineup.bat_lineup == this_line[sub_type])
-        sub_index = lineup.index[sub_filter]
-    elif sub_type == 'fielding':
-        sub_filter = (lineup.team_id == this_line['team_id']) & \
-                     (lineup.fielding == this_line[sub_type])
-        sub_index = lineup.index[sub_filter]
-    else:
-        print('Substitution Error: Fielding Oper')
-        exit()
+    sub_filter = (lineup.team_id == this_line['team_id']) & \
+                 (lineup.bat_lineup == this_line['batting'])
+    sub_index = lineup.index[sub_filter]
 
     # replace the person in the lineup
     lineup.at[sub_index[0], 'player_id'] = pid
     lineup.at[sub_index[0], 'player_nm'] = this_line['name']
+    lineup.at[sub_index[0], 'fielding'] = field_pos
 
     return [lineup, sub_index, new_substitution]
