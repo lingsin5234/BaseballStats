516a8703093d0551ef5accbf12763bc8751da7d2
lingsin5234 <sinto.005234@gmail.com>
2020-01-05 11:32:36 -0500

    round the times

--- date_time.py
+++ date_time.py
@@ -6,21 +6,20 @@ def seconds_convert(total_time):
 
     hours = 0
     minutes = 0
-    seconds = 0
     out_str = ''
 
     # hours
     if total_time > 60*60:
         hours = total_time // 60*60
-        out_str = hours + ' hours, '
+        out_str = str(int(hours)) + ' hours, '
 
     # minutes
     if total_time > 60:
         minutes = (total_time - hours*60*60) // 60
-        out_str = out_str + minutes + ' minutes, and '
+        out_str = out_str + str(int(minutes)) + ' minutes, and '
 
     # seconds
     seconds = total_time - hours*60*60 - minutes*60
-    out_str = out_str + seconds + ' seconds.'
+    out_str = out_str + str(round(seconds, 5)) + ' seconds.'
 
     return out_str
