9816f57471d426b0795396a29795eefc1ff2a786
lingsin5234 <sinto.005234@gmail.com>
2020-02-21 03:53:21 -0500

    initial migration

--- models.py
+++ models.py
@@ -14,8 +14,8 @@ class Teams(models.Model):
 # Game Info
 class GameInfo(models.Model):
     game_id = models.IntegerField()
-    home_team = models.ForeignKey(Teams, null=True, on_delete=models.SET_NULL)
-    away_team = models.ForeignKey(Teams, null=True, on_delete=models.SET_NULL)
+    home_team = models.ForeignKey(Teams, null=True, on_delete=models.SET_NULL, related_name='_home_team')
+    away_team = models.ForeignKey(Teams, null=True, on_delete=models.SET_NULL, related_name='_away_team')
     game_info = models.CharField(max_length=100)
 
     def __str__(self):
@@ -35,26 +35,26 @@ class Players(models.Model):
 # Starters
 class Starters(models.Model):
     game_id = models.ForeignKey(GameInfo, null=True, on_delete=models.SET_NULL)
-    away_1 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_2 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_3 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_4 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_5 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_6 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_7 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_8 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_9 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    away_10 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_1 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_2 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_3 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_4 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_5 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_6 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_7 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_8 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_9 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
-    home_10 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL)
+    away_1 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_1')
+    away_2 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_2')
+    away_3 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_3')
+    away_4 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_4')
+    away_5 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_5')
+    away_6 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_6')
+    away_7 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_7')
+    away_8 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_8')
+    away_9 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_9')
+    away_10 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_away_10')
+    home_1 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_1')
+    home_2 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_2')
+    home_3 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_3')
+    home_4 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_4')
+    home_5 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_5')
+    home_6 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_6')
+    home_7 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_7')
+    home_8 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_8')
+    home_9 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_9')
+    home_10 = models.ForeignKey(Players, null=True, on_delete=models.SET_NULL, related_name='_home_10')
 
     def __str__(self):
         return self.game_id
