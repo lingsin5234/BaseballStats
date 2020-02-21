431cecf70ae3844742483c7ced7e5d49f7f4bf04
lingsin5234 <sinto.005234@gmail.com>
2020-02-21 05:52:06 -0500

    added GamePlay to admin page

--- admin.py
+++ admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import Team, GameInfo, Player, Starter
+from .models import Team, GameInfo, Player, Starter, GamePlay
 
 # Register your models here.
 admin.site.register(Team)
 admin.site.register(GameInfo)
 admin.site.register(Player)
 admin.site.register(Starter)
+admin.site.register(GamePlay)
