c5440e2ba507cec556db38f51147ef6e778ae16f
lingsin5234 <sinto.005234@gmail.com>
2020-02-22 13:27:41 -0500

    updated View to JSON output

--- views.py
+++ views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
+from django.forms.models import model_to_dict
+
 
 # main views
 def stats_view(request):
     pull_stats = StatCollect.objects.all()
+    stats = []
+    for p in pull_stats:
+        add = model_to_dict(pull_stats)
+        stats.append(add)
 
     context = {
-        'stats': pull_stats
+        'stats': json.dumps(stats, cls=DjangoJSONEncoder)
     }
 
     return render(request, 'viewStats.html', context)
