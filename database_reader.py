ee7f26f47e9eb15ef8762b178b11517e7e1d95cc
lingsin5234 <sinto.005234@gmail.com>
2020-03-05 13:12:16 -0700

    using SQLAlchemy

--- database_reader.py
+++ database_reader.py
@@ -1,25 +1,34 @@
 # this is a testing script to read databases for specific entries
 import sqlite3 as sql
+import sqlalchemy as sa
 
-conn = sql.connect('baseball.db')
-c = conn.cursor()
+# setup engine and database
+engine = sa.create_engine('sqlite:///baseball.db', echo=True)
 
-# select query
-# c.execute('''SELECT * FROM starters''')
-# c.execute('''SELECT COUNT(*) FROM starters''')
-
-# check if table exists
-try:
-    table_nm = 'starters'
-    value = '1'
-    sql_query = '''SELECT * FROM ''' + table_nm + ''' WHERE fielding = ?'''
-    print(sql_query)
-    c.execute(sql_query, value)
-except sql.Error as e:
-    print('ERROR')
-    print(e)
-    exit()
-
-# fetch results
-results = c.fetchall()
+# query
+c = engine.connect()
+results = c.execute('SELECT * FROM starters LIMIT 5').fetchall()
 print(results)
+
+# conn = sql.connect('baseball.db')
+# c = conn.cursor()
+#
+# # select query
+# # c.execute('''SELECT * FROM starters''')
+# # c.execute('''SELECT COUNT(*) FROM starters''')
+#
+# # check if table exists
+# try:
+#     table_nm = 'starters'
+#     value = '1'
+#     sql_query = '''SELECT * FROM ''' + table_nm + ''' WHERE fielding = ?'''
+#     print(sql_query)
+#     c.execute(sql_query, value)
+# except sql.Error as e:
+#     print('ERROR')
+#     print(e)
+#     exit()
+#
+# # fetch results
+# results = c.fetchall()
+# print(results)
--- db_setup.py
+++ db_setup.py
@@ -1,5 +1,6 @@
-# setting up the sqlite database
+# setting up the sqlite database using sqlalchemy
 import sqlite3 as sql
+import sqlalchemy as sa
 
 # constants
 starter_col = 'game_id text, player_id text, player_nm text, team_id int, team_name text, bat_lineup int, fielding int'
@@ -9,28 +10,5 @@ gameplay_col = 'game_id text, type text, inning int, half int, half_innings text
                '2B_after text, 3B_after text, runs_scored int, total_scored int'
 
 
-# setup database and tables
-def create_tables(table_nm, col_nm):
-
-    conn = sql.connect('baseball.db')
-    c = conn.cursor()
-
-    # first check if exists before creating
-    try:
-        c.execute('''SELECT * FROM ''' + table_nm)
-    except sql.Error:
-        try:
-            sql_query = '''CREATE TABLE ''' + table_nm + ''' (?)'''
-            print(sql_query)
-            c.execute(sql_query, col_nm)
-        except sql.Error as e:
-            print("Error on SELECT and CREATE TABLE for '", table_nm, "'.")
-            print(e)
-            exit()
-        conn.commit()
-    c.close()
-
-
-# create tables for starters, gameplay
-create_tables('starters', starter_col)
-create_tables('gameplay', gameplay_col)
+# setup engine and database
+engine = sa.create_engine('sqlite:///baseball.db', echo=True)
