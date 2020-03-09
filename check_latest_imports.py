90888e58cf87a8ef834e1eb4216f6f7e3f1e05d4
lingsin5234 <sinto.005234@gmail.com>
2020-03-09 10:06:33 -0600

    udpated checks to list missing teams

--- check_latest_imports.py
+++ check_latest_imports.py
@@ -11,12 +11,21 @@ if len(sys.argv) > 1:
 
     # query
     c = dbs.engine.connect()
-    results = c.execute("SELECT COUNT(*) FROM process_log WHERE data_year=?", year).fetchall()
+    query = "SELECT team_name FROM process_log WHERE data_year=? AND process_name='stat_processor'"
+    results = c.execute(query, year).fetchall()
 
     # num of roster files
     all_files = os.listdir('import/' + year)
     rosters = [f for f in all_files if '.ROS' in f]
-    if len(results) == len(rosters):
+
+    # list the team names from the roster files
+    teams = [x.replace(year+'.ROS', '') for x in rosters]
+    results = [r for r, in results]
+
+    if len(results) == len(teams):
         print("All teams processed, stats generated.")
     else:
+        # find missing teams
+        missing_teams = [t for t in teams if t not in results]
         print("Incomplete! Still missing teams.")
+        print(missing_teams)
