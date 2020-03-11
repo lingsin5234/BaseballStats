a49df1cbe5f36e381dfdba5f9e618edfca2d33b1
lingsin5234 <sinto.005234@gmail.com>
2020-03-11 08:36:08 -0600

    tested cripsy - doesn't work

--- forms.py
+++ forms.py
@@ -1,4 +1,7 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
+from crispy_forms.bootstrap import InlineRadios
 
 # constants
 YEARS = ((2019, 2019), (2018, 2018), (2017, 2017), (2016, 2016), (2015, 2015),
@@ -7,3 +10,12 @@ YEARS = ((2019, 2019), (2018, 2018), (2017, 2017), (2016, 2016), (2015, 2015),
 
 class GetYear(forms.Form):
     get_year = forms.ChoiceField(choices=YEARS, widget=forms.RadioSelect)
+
+    # doesn't seem to do anything below....
+    def __init__(self, *args, **kwargs):
+        super(GetYear, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            InlineRadios('get_year'),
+            ButtonHolder('submit', 'Submit', css_class='button white')
+        )
