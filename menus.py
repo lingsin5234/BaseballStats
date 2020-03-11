c90c0e3fce50d15fbfc6dfece18037787f0541f2
lingsin5234 <sinto.005234@gmail.com>
2020-03-11 12:04:54 -0600

    fixed menu options

--- menus.py
+++ menus.py
@@ -3,14 +3,13 @@ from django.urls import reverse
 from . import views as baseball_vw
 
 # add items to the menu
+Menu.add_item("main", MenuItem("Home",
+                               reverse(baseball_vw.home_page),
+                               weight=10))
 Menu.add_item("main", MenuItem("Run Jobs",
                                reverse(baseball_vw.run_jobs_view),
                                weight=10))
 Menu.add_item("main", MenuItem("View Stats",
-                               url="/",
-                               # reverse(""),
+                               reverse(baseball_vw.stats_view),
                                weight=10))
-Menu.add_item("main", MenuItem("Home",
-                               url="/",
-                               # reverse(""),
-                               weight=10))
+
