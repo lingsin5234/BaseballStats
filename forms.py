560e6ead19693364cc88767ea4bd539f5d2eade5
lingsin5234 <sinto.005234@gmail.com>
2020-03-11 09:24:46 -0600

    handled process_team form

--- forms.py
+++ forms.py
@@ -6,14 +6,29 @@ from crispy_forms.bootstrap import InlineRadios
 # constants
 YEARS = ((2019, 2019), (2018, 2018), (2017, 2017), (2016, 2016), (2015, 2015),
          (2014, 2014), (2013, 2013), (2012, 2012), (2011, 2011), (2010, 2010))
+TEAMS = (('TOR', 'TOR'), ('BOS', 'BOS'), ('ANA', 'ANA'), ('TEX', 'TEX'))
 
 
 class GetYear(forms.Form):
-    get_year = forms.ChoiceField(choices=YEARS, widget=forms.RadioSelect)
+    get_year = forms.ChoiceField(choices=YEARS, widget=forms.RadioSelect, required=True)
 
     # doesn't seem to do anything below....
     def __init__(self, *args, **kwargs):
         super(GetYear, self).__init__(*args, **kwargs)
+        self.helper = FormHelper(self)
+        self.helper.layout = Layout(
+            InlineRadios('get_year'),
+            ButtonHolder('submit', 'Submit', css_class='button white')
+        )
+
+
+class ProcessTeam(forms.Form):
+    process_year = forms.ChoiceField(choices=YEARS, widget=forms.RadioSelect, required=True)
+    get_team = forms.ChoiceField(choices=TEAMS, widget=forms.RadioSelect, required=True)
+
+    # doesn't seem to do anything below....
+    def __init__(self, *args, **kwargs):
+        super(ProcessTeam, self).__init__(*args, **kwargs)
         self.helper = FormHelper(self)
         self.helper.layout = Layout(
             InlineRadios('get_year'),
