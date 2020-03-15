08fd74a123f77172e27ed0fc42c6f9b1be765cfe
lingsin5234 <sinto.005234@gmail.com>
2020-03-15 09:36:47 -0600

    latest menu and header updates

--- menus.py
+++ menus.py
@@ -3,13 +3,6 @@ from django.urls import reverse
 from . import views as baseball_vw
 
 # add items to the menu
-Menu.add_item("main", MenuItem("Home",
-                               reverse(baseball_vw.home_page),
-                               weight=10))
-Menu.add_item("main", MenuItem("Run Jobs",
-                               reverse(baseball_vw.run_jobs_view),
-                               weight=10))
-Menu.add_item("main", MenuItem("View Stats",
-                               reverse(baseball_vw.stats_view),
-                               weight=10))
-
+Menu.add_item("baseball", MenuItem("Home", reverse(baseball_vw.home_page), weight=10))
+Menu.add_item("baseball", MenuItem("Run Jobs", reverse(baseball_vw.run_jobs_view), weight=10))
+Menu.add_item("baseball", MenuItem("View Stats", reverse(baseball_vw.stats_view), weight=10))
