9eba6e393022a379c44267bebe5f942ba5876aa7
lingsin5234 <sinto.005234@gmail.com>
2020-03-18 08:39:17 -0600

    changed to symbolic links

--- db_setup.py
+++ db_setup.py
@@ -1,12 +1,1 @@
-# setting up the sqlite database using sqlalchemy
-import sqlite3 as sql
-import sqlalchemy as sa
-from . import class_structure as cl
-
-# setup engine and database
-engine = sa.create_engine('sqlite:///baseball.db', echo=True)
-
-# safe to call multiple times as it will FIRST check for table presence
-cl.metadata.create_all(engine)
-
-
+C:/Users/Sinto/Documents/PycharmProjects/BaseballApp/baseball/oper/db_setup.py
