a628cb403db736b237168b38a3f4babf831c2f6b
lingsin5234 <sinto.005234@gmail.com>
2020-03-18 10:06:34 -0600

    updated Year choices in run jobs form

--- forms.py
+++ forms.py
@@ -3,12 +3,14 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Fieldset, ButtonHolder, Submit
 from crispy_forms.bootstrap import InlineRadios
 from .models import JobRequirements
+from .oper import check_latest_imports as chk
 
 
 class GetYear(forms.ModelForm):
 
-    year_choices = [(c.year, c.year) for c in JobRequirements.objects.filter(form_type='import_year')]
-
+    missing_years = chk.check_years()
+    missing_years.reverse()
+    year_choices = [(m, m) for m in missing_years]
     year = forms.ChoiceField(required=True, label='Year', choices=year_choices)
 
     class Meta:
