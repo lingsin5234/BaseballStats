ab82e66b82dc43cb9ba6de9747225ed3819957d8
lingsin5234 <sinto.005234@gmail.com>
2020-04-06 21:15:00 -0600

    completed first job!

---
+++ jobs/hourly/importYear.py
@@ -0,0 +1,28 @@
+from django_extensions.management.jobs import HourlyJob, MonthlyJob
+from ...oper import import_retrosheet as ir
+from ...oper import error_logger as el
+from ...oper import check_latest_imports as chk
+
+
+# this job imports a certain Year from the retrosheet.org
+# Once tested, this should run MONTHLY
+class Job(MonthlyJob):
+    help = "Imports a year of files from Retrosheet.org"
+
+    def execute(self):
+
+        # get a list of the years not imported yet
+        year_choices = chk.check_years()
+        print(year_choices)
+        year = max(year_choices)
+        print(year)
+        status = ir.import_data(str(year))
+
+        # these messages should be stored in /var/log/syslog
+        # use cat syslog | grep CRON to view
+        if status:
+            print("Import Year... Success")
+            return True
+        else:
+            print("Import Year... Failed")
+            return False
