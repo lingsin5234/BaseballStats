67b2facfbf4aaedca24aaed6fa61f9ba8737caa9
lingsin5234 <sinto.005234@gmail.com>
2020-04-10 17:29:27 -0600

    latest testing to aggregate teh stats in the database

---
+++ baseball/oper/aggregate_statistics.py
@@ -0,0 +1,24 @@
+# this python file is for the job that aggregates all the STATS data by player_id, team_name and data_year
+from . import database_reader as dr
+from . import class_structure as cl
+import pandas as pd
+
+
+# aggregation function
+def stats_aggregate():
+
+    # start with batting stats
+    query = "SELECT * FROM batting"
+    results = dr.baseball_db_reader(query)
+    bat_col = [str(c).replace('batting.', '') for c in cl.batting_stats.columns]
+    df = pd.DataFrame(results, columns=bat_col)
+
+    # remove Id column and aggregate sum all columns, group by player_id, data_year, team_name
+    agg_col = [c for c in bat_col if c not in ['Id', 'player_id', 'data_year', 'team_name']]
+    print(agg_col)
+    # agg_sum = ["sum"] * len(agg_col)
+    df = df.groupby(['player_id', 'data_year', 'team_name'])[agg_col].sum(axis=1)\
+        .sort_values(by=['player_id', 'data_year', 'team_name']).reset_index()
+    print(df[df['team_name'] == 'ATL'])
+
+    return True
--- baseball/oper/running_tests.py
+++ baseball/oper/running_tests.py
@@ -2,13 +2,28 @@
 from . import import_retrosheet as ir
 from . import process_imports as pi
 from . import generate_statistics as gs
+from . import aggregate_statistics as ag
+from . import database_reader as dr
+import os
 
-
+'''
 # import Year; print True if completed
 print("Import Completed: ", ir.import_data(2018))
 
 # print True if this succeeds
-print("Processing Completed: ", pi.process_data_single_team(2018, 'TOR'))
+files = os.listdir('baseball/import/2018')
+files = [f[4:7] for f in files if f.find('ROS') == -1 and f.find('TEAM') == -1]
+for f in files:
+    # print("Processing Completed: ", f, pi.process_data_single_team(2018, f))
+    # print(f)
+    print("Stats Generated: ", f, gs.generate_stats(2018, f))
+'''
+'''
+# ^ above done wrong. should be processing ALL stats before doing a generate stats!
+query = "SELECT DISTINCT game_id FROM raw_player_stats"
+results = dr.baseball_db_reader(query)
+print(results)
+'''
+# print True if aggregate_statistics is completed
+print("Statistics Aggregated: ", ag.stats_aggregate())
 
-# print True if generating stats succeeds
-print("Stats Generated: ", gs.generate_stats(2018, 'TOR'))
