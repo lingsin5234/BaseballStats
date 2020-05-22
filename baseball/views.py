9e6af82bbe3480c9fafe709ab5c0c06360cd0103
lingsin5234 <sinto.005234@gmail.com>
2020-05-22 11:25:12 -0600

    fix the loop created by extract_team_player

--- baseball/views.py
+++ baseball/views.py
@@ -5,9 +5,6 @@ from flask import jsonify
 from django.core.serializers.json import DjangoJSONEncoder
 from .models import StatCollect, JobRequirements
 from .oper import db_setup as dbs
-from .oper import import_retrosheet as ir
-from .oper import process_imports as pi
-from .oper import generate_statistics as gs
 from .oper import check_latest_imports as chk
 from .oper import database_reader as dr
 from .oper import class_structure as cs
@@ -147,89 +144,7 @@ def stats_view(request):
     return render(request, 'pages/viewStats.html', context)
 
 
-# run jobs View
-def run_jobs_view(request):
-
-    # DB connect and form initializations
-    c = dbs.engine.connect()
-
-    # instantiate all forms
-    form_import, form_process, form_gen_stats = instantiate_forms()
-
-    if request.method == 'POST':
-        if 'team' in request.POST:
-            team = request.POST['team']
-        year = request.POST['year']
-        if request.POST['form_type'] == 'import_year':
-
-            year_choices = chk.get_year_choices()[1]
-            form = GetYear(year_choices, request.POST)
-
-            if form.is_valid():
-                status = ir.import_data(request.POST['year'])
-                if status:
-                    messages.info(request, "Import Completed for " + year)
-
-                    # re-run all the forms!
-                    form_import, form_process, form_gen_stats = instantiate_forms()
-                else:
-                    messages.info(request, year + ' could not be imported')
-            else:
-                print(form.errors)
-        elif request.POST['form_type'] == 'process_team':
-
-            year_choices = chk.get_team_choices('process_team')[1]
-            team_choices = chk.get_team_choices('process_team')[3]
-            form = ProcessTeam(year_choices, team_choices, request.POST)
-
-            if form.is_valid():
-                status = pi.process_data_single_team(year, team)
-                if status:
-                    messages.info(request, "Processed " + team + " for " + year)
-
-                    # re-run all the forms!
-                    form_import, form_process, form_gen_stats = instantiate_forms()
-                else:
-                    messages.info(request, 'Year has not been Imported.')
-            else:
-                print(form.errors)
-        elif request.POST['form_type'] == 'generate_stats':
-
-            year_choices = chk.get_team_choices('generate_stats')[1]
-            team_choices = chk.get_team_choices('generate_stats')[3]
-            form = GenerateStats(year_choices, team_choices, request.POST)
-
-            if form.is_valid():
-                status = gs.generate_stats(year, team)
-                if status:
-                    messages.info(request, "Generated Statistics for " + team + " in " + year)
-
-                    # re-run all the forms!
-                    form_import, form_process, form_gen_stats = instantiate_forms()
-                else:
-                    messages.info(request, 'Year has not been Imported OR Team File has not been processed.')
-            else:
-                print(form.errors)
-    else:
-        # do nothing as initial forms are already declared
-        pass
-
-    query = "SELECT * FROM process_log ORDER BY timestamp DESC LIMIT 10"
-    results = [dict(row) for row in c.execute(query).fetchall()]
-
-    # columns wanted
-    columns = [{'col': 'process_name'}, {'col': 'data_year'}, {'col': 'team_name'},
-               {'col': 'time_elapsed'}, {'col': 'timestamp'}]
-
-    context = {
-        'results': json.dumps(results),
-        'form_import': form_import,
-        'form_process': form_process,
-        'form_gen_stats': form_gen_stats,
-        'columns': json.dumps(columns)
-    }
-
-    return render(request, 'pages/runJobs.html', context)
+# run jobs View -- REMOVED 20200522
 
 
 # this will replace the runJobs with a dashboard of the automated jobs
