7589690c643bfa3a3dabcf0dd0983c93ac1ce244
lingsin5234 <sinto.005234@gmail.com>
2020-06-14 18:38:53 -0600

    run tests on baserunners and fielder's choice

--- baseball/oper/running_tests.py
+++ baseball/oper/running_tests.py
@@ -7,6 +7,7 @@ from . import database_reader as dr
 from . import fix_quotes_in_names as fqn
 from . import db_setup as dbs
 import os
+import re
 
 '''
 # import Year; print True if completed
@@ -33,7 +34,7 @@ print(results)
 # test run for removing strings
 fqn.remove_quotes_fix()
 '''
-
+'''
 # Check the gameplay table
 query = "SELECT * FROM gameplay LIMIT 10"
 results = dr.baseball_db_reader(query)
@@ -46,4 +47,29 @@ print(results)
 
 # Show all tables
 print(dbs.engine.table_names())
+'''
+
+#  -----  Fielder's Choice Fix  -----  #
+# load the imports for 2018 then find FC
+FC = []
+dir_str = 'baseball/import/2018'
+all_files = os.listdir(dir_str)
+
+for file_nm in all_files:
+    # get current file
+    file_dir = dir_str + '/' + file_nm
+    f = open(file_dir, "r")
+    f1 = f.readlines()
+
+    for line_item in f1:
+        if line_item[0:4] == 'play':
+            play = line_item.split(',')
+            play = play[len(play)-1].replace('\n', '')
+            if bool(re.search(r'X[123H]\(', play)):
+                FC.append(play)
+
+FC = list(set(FC))
+for f in FC:
+    print(f)
+#  ----------------------------------  #
 
