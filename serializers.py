615c84aaab91b4902ae38e4321d5e0f220b4b821
lingsin5234 <sinto.005234@gmail.com>
2020-06-25 17:38:48 -0600

    viewStats is production ready

--- serializers.py
+++ serializers.py
@@ -2,10 +2,6 @@ from rest_framework import serializers
 
 
 class StatSerializer(serializers.Serializer):
-    # Your data serializer, define your fields here.
-    # results = serializers.ListField()
-    # heading = serializers.CharField()
-    # post_col = serializers.ListField()
     NAME = serializers.CharField()
     TEAM = serializers.CharField()
     GP = serializers.IntegerField()
